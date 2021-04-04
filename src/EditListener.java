4ab65f8ee9c07f11a21852f14869f20563c5d472
ratang <amianijohns@gmail.com>
2021-04-04 19:18:18 -0400

    initial commit

---
+++ src/EditListener.java
@@ -0,0 +1,14 @@
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class EditListener implements DocumentListener {
+    public void insertUpdate(DocumentEvent e) {
+
+    }
+
+    public void removeUpdate(DocumentEvent e) {
+
+    }
+
+    public void changedUpdate(DocumentEvent e) {}
+}
