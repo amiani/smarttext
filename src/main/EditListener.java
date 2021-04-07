1820ac540480514172a1130efaee596e5377df9a
ratang <amianijohns@gmail.com>
2021-04-07 17:15:27 -0400

    add EditListener insertUpdate

--- src/main/EditListener.java
+++ src/main/EditListener.java
@@ -2,6 +2,8 @@ package main;
 
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 public class EditListener implements DocumentListener {
     EditManager editManager;
@@ -10,7 +12,15 @@ public class EditListener implements DocumentListener {
     }
 
     public void insertUpdate(DocumentEvent e) {
-
+        Document doc = e.getDocument();
+        try {
+            int position = e.getOffset();
+            String text = doc.getText(position, e.getLength());
+            editManager.insertString(position, text);
+            System.out.println(position + ": " + text);
+        } catch (BadLocationException badLocationException) {
+            badLocationException.printStackTrace();
+        }
     }
 
     public void removeUpdate(DocumentEvent e) {
