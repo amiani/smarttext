6f01a123236c7f0ad69dfd40d14a12c36a86fced
ratang <amianijohns@gmail.com>
2021-04-13 16:12:04 -0400

    collapse imports

--- src/main/EditManager.java
+++ src/main/EditManager.java
@@ -1,9 +1,6 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
