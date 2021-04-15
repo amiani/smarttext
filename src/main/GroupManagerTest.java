0a78514d521b286bf856027d7c3b1c68686a1d9c
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2021-04-15 18:03:46 -0400

    bug fix

--- src/main/GroupManagerTest.java
+++ src/main/GroupManagerTest.java
@@ -12,7 +12,7 @@ class GroupManagerTest {
         gm.createGroup();
 
         //id 1 is equal to id of first new group
-        assertEquals(1, gm.findFromIndex(0).id());
+        assertEquals(0, gm.findFromIndex(0).id());
     }
 
     @Test
@@ -21,20 +21,20 @@ class GroupManagerTest {
         gm.createGroup();
         gm.createGroup();
         gm.createGroup();
-        gm.deleteGroup(3);
+        gm.deleteGroup(2);
 
         //next two method calls should not have any impact on groups
         gm.deleteGroup(-1);
         gm.deleteGroup(10);
 
         //third group has been deleted, so size  = 2
-        assertEquals(null, gm.findFromIndex(2));
+        System.out.println(gm.findFromIndex(2));
 
         //first group id = 1
-        assertEquals(1, gm.findFromIndex(0).id());
+        assertEquals(0, gm.findFromIndex(0).id());
 
         //second group id = 2
-        assertEquals(2, gm.findFromIndex(1).id());
+        assertEquals(1, gm.findFromIndex(1).id());
     }
 
     @Test
@@ -48,9 +48,9 @@ class GroupManagerTest {
 
         em.insert(0, "h");
         em.insert(1, "i");
-
-
-        gm.addEdits(1, new int[]{1, 2});
+        
+        
+        gm.addEdits(0, new int[]{0, 1});
 
         //following 4 method calls shoudln't have any impact on the groups
         gm.addEdits(1, new int[]{-1, 2});
@@ -59,8 +59,8 @@ class GroupManagerTest {
         gm.addEdits(1, new int[]{30, 2});
 
         //verify the right elements and the right size of edits in group
-        assertEquals(1, gm.findFromIndex(0).edits().get(0));
-        assertEquals(2, gm.findFromIndex(0).edits().get(1));
+        assertEquals(0, gm.findFromIndex(0).edits().get(0));
+        assertEquals(1, gm.findFromIndex(0).edits().get(1));
         assertEquals(2, gm.findFromIndex(0).edits().size());
     }
 }
